015eb002391265489f73c1d5f39829a382b64e31
Dell e5470 <khachatryan-aghasi@mail.ru>
2021-06-16 15:12:34 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        boolean x = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter minute");
+        int minute = scanner.nextInt();
+        if (minute > 60 || minute < 0) {
+            System.out.println("Minute cannot be more than 60 and less than 0");
+            System.out.println("Enter minute");
+            minute = scanner.nextInt();
+        }
+        System.out.println("Enter second");
+        int second = scanner.nextInt();
+        if (second > 60 || second < 0) {
+            System.out.println("Minute cannot be more than 60 and less than 0");
+            System.out.println("Enter second");
+            second = scanner.nextInt();
+        }
+        System.out.println("Timer:");
+        while (x) {
+
+            System.out.println(minute + "m : " + second + "s");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            second--;
+            if (second < 0) {
+                second = 60;
+                minute--;
+                if (minute < 0) {
+                    System.out.println("Time is over!");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
